b5baff8f68b4a725eaba3a5b517d14b5c65bd686
AGASS007 <devnull@localhost>
2012-04-19 14:25:17 +0200

    Ajout des templates (oubliées au précédent commit). Amélioration de l'URLConf => simplification de l'appel des pages dans les templates via {% url ... %}

--- newco/apps/pp/urls.py
+++ newco/apps/pp/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
+from pp.models import DProduct
 
-
-urlpatterns = patterns('',
-    url(r'^products/$', 'pp.views.index'),
-    url(r'^products/(?P<product_id>\d+)/$', 'pp.views.detail'),
+urlpatterns = patterns('pp.views',
+    url(r'^products/$', 'index', name='product_index'),
+    url(r'^products/product/(?P<product_id>\d+)/$', 'detail', name='product_detail'),
 )
