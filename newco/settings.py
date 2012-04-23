fad13b9bbea2aeb4c469ca7baaad00e3312a5936
Maxime Rouyrre <rouyrre+newco@gmail.com>
2012-04-23 20:54:32 +0200

    remove MySQL backend

--- newco/settings.py
+++ newco/settings.py
@@ -29,17 +29,6 @@ MANAGERS = ADMINS
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql", # Add "postgresql_psycopg2", "postgresql", "mysql", "sqlite3" or "oracle".
-        "NAME": "djtest",                       # Or path to database file if using sqlite3 dev.db.
-        "USER": "djangouser",                             # Not used with sqlite3.
-        "PASSWORD": "password",                         # Not used with sqlite3.
-        "HOST": "",                             # Set to empty string for localhost. Not used with sqlite3.
-        "PORT": "",                             # Set to empty string for default. Not used with sqlite3.
-    }
-}
-
-DATABASES = {
-    "default": {
         "ENGINE": "django.db.backends.sqlite3", # Add "postgresql_psycopg2", "postgresql", "mysql", "sqlite3" or "oracle".
         "NAME": "dev.db",                       # Or path to database file if using sqlite3 dev.db.
         "USER": "",                             # Not used with sqlite3.
