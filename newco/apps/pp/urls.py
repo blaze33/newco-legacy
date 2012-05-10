3be23ccb20599170c6debd3cc1ca7de21abe8f86
Guillaume Andreu Sabater <guillaume.andreu.sabater@gmail.com>
2012-05-10 16:28:17 +0200

    Switch pp module to CBV

--- newco/apps/pp/urls.py
+++ newco/apps/pp/urls.py
@@ -1,16 +1,18 @@
 from django.conf.urls.defaults import patterns, url, include
 from django.views.generic import ListView
-from pp.models import DProduct
+from configurableproduct.models import ProductType
+
+from pp.views import ProductByPTListView, ProductDetailView
 
 
-urlpatterns = patterns('pp.views',
-    url(r'^products/$', 'index', name='product_index'),
-    url(r'^products/product/(?P<product_id>\d+)/$', 'detail', name='product_detail'),
+urlpatterns = patterns('',
+    url(r'^products/$', ListView.as_view(model=ProductType,
+                                context_object_name="producttype_list")),
 )
 
 urlpatterns += patterns('',
-    url(r'^products2/$', ListView.as_view(
-        model=DProduct,
-        context_object_name="product_list",
-    )),
+    url(r'^products/(\w+)/$', ProductByPTListView.as_view(),
+            name='producttype_detail'),
+    url(r'^products/product/(?P<pk>\d+)/$', ProductDetailView.as_view(),
+            name='product_detail'),
 )
--- newco/apps/pp/views.py
+++ newco/apps/pp/views.py
@@ -1,31 +1,36 @@
-from django.template import RequestContext
-from django.shortcuts import render_to_response, get_object_or_404
+from django.shortcuts import get_object_or_404
 
-from django.views.generic import DetailView
+from django.views.generic import ListView, DetailView
 
 from pp.models import DProduct
 from configurableproduct.models import ProductType
 
-def index(request):
-    product_list = DProduct.objects.all()
-    producttype_list = ProductType.objects.all()
-    return render_to_response('pp/index.html',
-                              {'product_list': product_list, 'producttype_list': producttype_list},
-                              context_instance=RequestContext(request))
 
-def detail(request, product_id):
-    p = get_object_or_404(DProduct, pk=product_id)
-    return render_to_response('pp/detail.html', {'product': p},
-                              context_instance=RequestContext(request))
+class ProductByPTListView(ListView):
 
-#class ProductDetailView(DetailView):
+    context_object_name = "product_list"
+    template_name = "pp/products_by_producttype.html"
 
-#    context_object_name = "dproduct"
-#    model = DProduct
+    def get_queryset(self):
+        self.producttype = get_object_or_404(ProductType,
+                                             name__iexact=self.args[0])
+        return DProduct.objects.filter(type=self.producttype)
 
-#    def get_context_data(self, **kwargs):
-#        # Call the base implementation first to get a context
-#        context = super(ProductDetailView, self).get_context_data(**kwargs)
-#        # Add in a QuerySet of all the books
-##        context['book_list'] = Book.objects.all()
-#        return context
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(ProductByPTListView, self).get_context_data(**kwargs)
+        # Add in the publisher
+        context['producttype'] = self.producttype
+        return context
+
+
+class ProductDetailView(DetailView):
+
+    context_object_name = "product"
+    queryset = DProduct.objects.all()
+
+    def get_object(self):
+        # Call the superclass
+        object = super(ProductDetailView, self).get_object()
+        # Return the object
+        return object
