8da8a28dd71b9d9d1466c89e956268bdff6de55f
Maxime Rouyrre <rouyrre+newco@gmail.com>
2012-05-21 19:04:20 +0200

    Item deletion is implemented. Delete buttons only visible to superusers.

--- newco/apps/items/urls.py
+++ newco/apps/items/urls.py
@@ -9,4 +9,6 @@ urlpatterns = patterns('',
     url(r"^(?P<model_name>[-\w]+)/(?P<pk>\d+)/(?P<slug>[-\w]+)/$", ContentDetailView.as_view(), name="item_detail"),
     url(r"^add/(?P<model_name>[-\w]+)/$", ContentCreateView.as_view(), name="item_create"),
     url(r"^edit/(?P<model_name>[-\w]+)/(?P<pk>\d+)/$", ContentUpdateView.as_view(), name="item_edit"),
+    url(r"^delete/(?P<model_name>[-\w]+)/(?P<pk>\d+)/$", ContentDeleteView.as_view(),
+            name="item_delete"),
 )
--- newco/apps/items/views.py
+++ newco/apps/items/views.py
@@ -2,10 +2,10 @@
 from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response, get_object_or_404
-from django.views.generic import View, FormView, ListView, CreateView, DetailView, UpdateView
+from django.views.generic import View, FormView, ListView, CreateView, DetailView, UpdateView, DeleteView
 from items.models import *
 from django.db.models.loading import get_model
-from django.core.urlresolvers import resolve
+from django.core.urlresolvers import resolve, reverse
 
 app_name = 'items'
 
@@ -19,3 +19,10 @@ class ContentCreateView(ContentView, CreateView): pass
 class ContentUpdateView(ContentView, UpdateView): pass
 class ContentDetailView(ContentView, DetailView): pass
 class ContentListView(ContentView, ListView): pass
+class ContentDeleteView(ContentView, DeleteView):
+    def get_success_url(self):
+        model_name = self.model.__name__
+        if model_name == "Question":
+            return self.object.item.get_absolute_url()
+        else:
+            return reverse("item_index")
