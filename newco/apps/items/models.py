9543bc420b9560ca6ca1d8048a49887c54947986
Maxime Rouyrre <rouyrre+newco@gmail.com>
2012-05-24 20:31:39 +0200

    Ask a question is implemented using FormSets.

--- newco/apps/items/models.py
+++ newco/apps/items/models.py
@@ -5,6 +5,8 @@ from taggit.managers import TaggableManager
 from django.db.models import permalink
 from django.template.defaultfilters import slugify
 from django.forms import ModelForm
+import datetime
+from django.forms.widgets import Textarea
 
 class Item(models.Model):
     name = models.CharField(max_length=255, verbose_name=_('Name'))
@@ -29,9 +31,14 @@ class Item(models.Model):
 
 class Question(models.Model):
     content = models.CharField(max_length=200)
-    pub_date = models.DateTimeField(_('date published'))
+    pub_date = models.DateTimeField(default=datetime.datetime.today(),
+                                    editable=False,
+                                    verbose_name=_('date published'))
     author = models.ForeignKey(User)
     items = models.ManyToManyField(Item)
+    
+    def __unicode__(self):
+        return u'%s' % (self.content)
 
 class Answer(models.Model):
     question = models.ForeignKey(Question)
@@ -43,4 +50,25 @@ class Story(models.Model):
     title = models.CharField(max_length=200)
     content = models.CharField(max_length=2000)
     items = models.ManyToManyField(Item)
+    
+class QuestionForm(ModelForm):
+    class Meta:
+        model = Question
+        exclude = ('author', 'items')
+        widgets = {
+            'content': Textarea(attrs={'class': 'span4', 'placeholder': 'Ask something specific. Be concise.', 'rows': 1}),
+        }
 
+    def save(self, request, commit=True, **kwargs):
+        question = super(QuestionForm, self).save(commit=False)
+        question.author = request.user
+        question.save()
+        question.items.add(kwargs.pop('pk'))
+
+class AnswerForm(ModelForm):
+    class Meta:
+        model = Answer
+
+class StoryForm(ModelForm):
+    class Meta:
+        model = Story
--- newco/apps/items/views.py
+++ newco/apps/items/views.py
@@ -1,13 +1,15 @@
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.template import RequestContext
 from django.shortcuts import render_to_response, get_object_or_404
 from django.views.generic import View, FormView, ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.views.generic.edit import ProcessFormView, FormMixin
 from items.models import *
 from django.db.models.loading import get_model
 from django.core.urlresolvers import resolve, reverse
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import permission_required
+from django.forms.models import formset_factory
 
 app_name = 'items'
 
@@ -19,7 +21,41 @@ class ContentView(View):
 
 class ContentCreateView(ContentView, CreateView): pass
 class ContentUpdateView(ContentView, UpdateView): pass
-class ContentDetailView(ContentView, DetailView): pass
+class ContentDetailView(ContentView, DetailView, ProcessFormView, FormMixin):
+
+    def get_context_data(self, **kwargs):
+        context = super(ContentDetailView, self).get_context_data(**kwargs)
+        self.object = self.get_object()
+        if self.model == Item:
+            QuestionFormSet = formset_factory(QuestionForm)
+            if self.request.POST:
+                fs = QuestionFormSet(self.request.POST, prefix='questions')
+            else:
+                fs = QuestionFormSet(prefix='questions')
+            context['formset'] = fs
+            context['item'] = self.object
+        return context
+
+    def form_invalid(self, form):
+        return self.render_to_response(self.get_context_data(form=form))
+
+    def form_valid(self, formset, request, **kwargs):
+        for form in formset:
+            if form.cleaned_data:
+                self.object = form.save(request, **kwargs)
+                form.save_m2m()     
+        return HttpResponseRedirect(self.get_object().get_absolute_url())
+
+    def post(self, request, *args, **kwargs):
+        if 'question_ask' in request.POST:
+            self.formset_class = formset_factory(QuestionForm)
+            formset = self.formset_class(data=self.request.POST,
+                                         prefix='questions')
+            if formset.is_valid():
+                return self.form_valid(formset, request, **kwargs)
+        else:
+            return self.form_invalid(form)
+    
 class ContentListView(ContentView, ListView): pass
 
 class ContentDeleteView(ContentView, DeleteView):
