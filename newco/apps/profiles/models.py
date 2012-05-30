c35d347fab41adec443edacffa7289f0a6f4640a
sebastien_worms <devnull@localhost>
2012-05-31 01:29:06 +0200

    Added subscription_date to profiles_profile & profile view

--- newco/apps/profiles/models.py
+++ newco/apps/profiles/models.py
@@ -3,9 +3,14 @@ from django.utils.translation import ugettext_lazy as _
 
 from idios.models import ProfileBase
 
+import datetime
+
 
 class Profile(ProfileBase):
     name = models.CharField(_("name"), max_length=50, null=True, blank=True)
     about = models.TextField(_("about"), null=True, blank=True)
     location = models.CharField(_("location"), max_length=40, null=True, blank=True)
     website = models.URLField(_("website"), null=True, blank=True, verify_exists=False)
+    subscription_date = models.DateTimeField(default=datetime.datetime.today(),
+                                    editable=False,
+                                    verbose_name=_('date published'))
