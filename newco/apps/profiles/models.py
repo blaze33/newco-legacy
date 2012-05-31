b078575de381e9f2bc58ee6b63f681b5581ca8c2
sebastien_worms <devnull@localhost>
2012-05-31 18:58:07 +0200

    Added Subscription date to profile Creation of reputation class, visualization in profile, initialization of class at sign up/ user creation (through postsave signal)

--- newco/apps/profiles/models.py
+++ newco/apps/profiles/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+from django.db.models.signals import post_save
+
 from idios.models import ProfileBase
+from django.contrib.auth.models import User
 
 import datetime
-
 
 class Profile(ProfileBase):
     name = models.CharField(_("name"), max_length=50, null=True, blank=True)
@@ -14,3 +16,15 @@ class Profile(ProfileBase):
     subscription_date = models.DateTimeField(default=datetime.datetime.today(),
                                     editable=False,
                                     verbose_name=_('date published'))
+
+
+class Reputation(models.Model):
+    user = models.OneToOneField(User)
+    reputation_value = models.IntegerField(default=0)
+    
+    
+def create_reputation(sender, instance=None, **kwargs):
+    if instance is None:
+        return
+    reputation, created = Reputation.objects.get_or_create(user=instance)
+post_save.connect(create_reputation, sender=User)
