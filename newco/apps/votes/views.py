33ca2c9049e390a74be4bfe5a1f53818fe8d68c5
Guillaume Andreu Sabater <guillaume.andreu.sabater@gmail.com>
2012-05-31 18:24:24 +0200

    adding of login_required decorator on votes.views

--- newco/apps/votes/views.py
+++ newco/apps/votes/views.py
@@ -1,11 +1,12 @@
-from django.http import Http404
+from django.http import HttpResponseRedirect
 from django.db.models.loading import get_model
+from django.contrib.auth.decorators import login_required
 
 from voting.views import vote_on_object
 
 APP_NAME = 'items'
 
-
+@login_required
 def rate_object(request, model_name, object_id):
 
     model = get_model(APP_NAME, model_name)
@@ -14,4 +15,11 @@ def rate_object(request, model_name, object_id):
         return vote_on_object(request, model, request.POST['vote_button'],
                 object_id=object_id)
     else:
-        return Http404
+        lookup_kwargs = {}
+        lookup_kwargs['%s__exact' % model._meta.pk.name] = object_id
+        try:
+            obj = model._default_manager.get(**lookup_kwargs)
+        except ObjectDoesNotExist:
+            return json_error_response(
+                'No %s found for %s.' % (model._meta.verbose_name, lookup_kwargs))
+        return HttpResponseRedirect(obj.get_absolute_url())
