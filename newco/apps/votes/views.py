9899c37b1677ae466aaeaa1788abfec8f738fce1
Guillaume Andreu Sabater <guillaume.andreu.sabater@gmail.com>
2012-05-31 19:04:52 +0200

    fixing of error handling in votes.views

--- newco/apps/votes/views.py
+++ newco/apps/votes/views.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponseRedirect
 from django.db.models.loading import get_model
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
 
 from voting.views import vote_on_object
 
 APP_NAME = 'items'
+
 
 @login_required
 def rate_object(request, model_name, object_id):
@@ -17,9 +19,11 @@ def rate_object(request, model_name, object_id):
     else:
         lookup_kwargs = {}
         lookup_kwargs['%s__exact' % model._meta.pk.name] = object_id
+
         try:
             obj = model._default_manager.get(**lookup_kwargs)
         except ObjectDoesNotExist:
-            return json_error_response(
-                'No %s found for %s.' % (model._meta.verbose_name, lookup_kwargs))
+            raise AttributeError('No %s for %s.' % (model._meta.app_label,
+                                                    lookup_kwargs))
+
         return HttpResponseRedirect(obj.get_absolute_url())
