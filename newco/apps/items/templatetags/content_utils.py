6c777cf99f47f34b602a6fdb20b6c7eb88b965de
Guillaume Andreu Sabater <guillaume.andreu.sabater@gmail.com>
2012-06-15 14:18:38 +0200

    modifying of content_utils return module_name instead of class name

--- newco/apps/items/templatetags/content_utils.py
+++ newco/apps/items/templatetags/content_utils.py
@@ -2,12 +2,15 @@ from django import template
 
 register = template.Library()
 
+
 @register.filter
 def to_class_name(value):
-    return value.__class__.__name__
+    return value._meta.module_name
+
 
 @register.inclusion_tag('items/tag_edit.html')
 def edit(item_name, item_id):
-    return {'item_name': item_name,
-            'item_id': item_id,
-            }
+    return {
+        'item_name': item_name,
+        'item_id': item_id,
+    }
