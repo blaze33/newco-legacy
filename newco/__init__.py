e0cce400888aa325e506ec6ae507b326ebe7ea3d
Guillaume Andreu Sabater <guillaume.andreu.sabater@gmail.com>
2012-06-19 12:02:06 +0200

    removing of init overriding of DJANGO_SETTINGS_MODULE

--- newco/__init__.py
+++ newco/__init__.py
@@ -5,6 +5,3 @@ This project takes the account_project and adds profiles and notifications.
 It is a foundation suitable for many sites that have user accounts with
 profiles.
 """
-
-import os
-os.environ['DJANGO_SETTINGS_MODULE'] = 'newco.settings'
