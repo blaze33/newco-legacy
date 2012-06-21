b9189bcca16037d9fd8f9ce5d7e7b49d9cd40ba9
Guillaume Andreu Sabater <guillaume.andreu.sabater@gmail.com>
2012-06-21 21:50:04 +0200

    overriding more idios urls since they screwed up big time their url conf in the last release!!!!

--- newco/apps/profiles/urls.py
+++ newco/apps/profiles/urls.py
@@ -1,14 +1,20 @@
 from django.conf.urls import patterns, include, url
 
 from profiles.views import ProfileRedirectView, ProfileDetailView
+from idios.views import ProfileListView, ProfileUpdateView, ProfileCreateView
 
 
 urlpatterns = patterns('',
+    url(r"^$", ProfileListView.as_view(), name="profile_list"),
+    url(r"^all/$", ProfileListView.as_view(all_profiles=True),
+                                            name="profile_list_all"),
+
     url(r"^profile/(?P<username>[\w\._-]+)/$",
             ProfileRedirectView.as_view(), name="profile_detail"),
     url(r"^profile/(?P<username>[\w\._-]+)/(?P<slug>[\w\._-]+)/$",
             ProfileDetailView.as_view(), name="profile_detail_full"),
-    url(r"^(?P<profile_slug>[\w\._-]+)/profile/(?P<profile_pk>\d+)/$",
-            ProfileDetailView.as_view(), name="profile_detail_full"),
+
+    url(r"^edit/$", ProfileUpdateView.as_view(), name="profile_edit"),
+    url(r"^create/$", ProfileCreateView.as_view(), name="profile_create"),
     url(r"^", include("idios.urls")),
 )
