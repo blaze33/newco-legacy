05ab8d1cba5596d8552d297f734c444a0b3041fb
Guillaume Andreu Sabater <guillaume.andreu.sabater@gmail.com>
2012-06-21 20:53:38 +0200

    fixing admin login

--- newco/apps/custaccount/urls.py
+++ newco/apps/custaccount/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from django.contrib.auth.decorators import login_required
 
 from custaccount.views import LoginView, SignupView
+
+admin.autodiscover()
+admin.site.login = login_required(admin.site.login)
 
 
 urlpatterns = patterns("",
