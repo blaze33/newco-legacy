3fd10fb1d386efd6f2f524ef8aadba9f6fdf6b67
Guillaume Andreu Sabater <guillaume.andreu.sabater@gmail.com>
2012-06-21 22:50:45 +0200

    switching back to idios 0.2a1 since newer one not working.

--- newco/apps/profiles/urls.py
+++ newco/apps/profiles/urls.py
@@ -1,20 +1,12 @@
 from django.conf.urls import patterns, include, url
 
 from profiles.views import ProfileRedirectView, ProfileDetailView
-from idios.views import ProfileListView, ProfileUpdateView, ProfileCreateView
 
 
 urlpatterns = patterns('',
-    url(r"^$", ProfileListView.as_view(), name="profile_list"),
-    url(r"^all/$", ProfileListView.as_view(all_profiles=True),
-                                            name="profile_list_all"),
-
     url(r"^profile/(?P<username>[\w\._-]+)/$",
             ProfileRedirectView.as_view(), name="profile_detail"),
     url(r"^profile/(?P<username>[\w\._-]+)/(?P<slug>[\w\._-]+)/$",
             ProfileDetailView.as_view(), name="profile_detail_full"),
-
-    url(r"^edit/$", ProfileUpdateView.as_view(), name="profile_edit"),
-    url(r"^create/$", ProfileCreateView.as_view(), name="profile_create"),
     url(r"^", include("idios.urls")),
 )
