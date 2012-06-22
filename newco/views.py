66a9e30727ac744486cd7e567190240140d3d874
sebastien_worms <devnull@localhost>
2012-06-22 18:36:01 +0200

    De-massacring the HomepageRedirectView ... Sorry for the mess
    Small change in the Homepage_logged : display Newsfeed instead of history as default

--- newco/views.py
+++ newco/views.py
@@ -5,20 +5,18 @@ from django.views.generic.base import RedirectView, TemplateView
 from profiles.views import ProfileDetailView
 
 
-class HomePageRedirectView(RedirectView, TemplateView, ProfileDetailView):#TemplateView
+class HomePageRedirectView(RedirectView, TemplateView):
 
     def get(self, request, *args, **kwargs):
         if request.user.is_authenticated():
-            context = ProfileDetailView.get_context_data(**kwargs)
-#            url = "/content/"
-#            url = request.user.get_profile().get_absolute_url()
-#            if url:
-#                if self.permanent:
-#                    return HttpResponsePermanentRedirect(url)
-#                else:
-#                    return HttpResponseRedirect(url)
-#            else:
-#                return HttpResponseGone()
+            url = request.user.get_profile().get_absolute_url()
+            if url:
+                if self.permanent:
+                    return HttpResponsePermanentRedirect(url)
+                else:
+                    return HttpResponseRedirect(url)
+            else:
+                return HttpResponseGone()
         else:
             self.template_name = "homepage.html"
             context = self.get_context_data(**kwargs)
