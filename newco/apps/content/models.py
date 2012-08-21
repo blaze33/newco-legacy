c40e91010afd5c011bf764f73dbdf1593e0d2bfb
Maxime Rouyrre <rouyrre+newco@gmail.com>
2012-08-22 01:21:20 +0200

    Content app refactoring: - Ditch genre class, it's an Item - Do not depend on Content from old items app - Use a basemodel for timestamps and vote. TODO: get independance from vote

--- newco/apps/content/models.py
+++ newco/apps/content/models.py
@@ -1,41 +1,57 @@
-from django.db import models
+from django_extensions.db.models import TimeStampedModel
 from django_hstore import hstore
-from items.models import Content
 from django.utils.translation import ugettext_lazy as _
+from django.contrib.contenttypes import generic
+from voting.models import Vote
 
-# Create your models here.
-class Genre(models.Model):
-    name  = models.CharField(max_length=200, verbose_name=_("name"))
-    description = models.CharField(max_length=1000, verbose_name=_("description"))
+
+class BaseModel(TimeStampedModel):
+    """ BaseModel
+    An abstract base class with timestamps and votes.
+    """
+    votes = generic.GenericRelation(Vote)
 
     class Meta:
-        verbose_name = _("genre")
+        abstract = True
 
-    def __unicode__(self):
-        return self.name
+    def delete(self):
+        try:
+            self.votes.all().delete()
+        except:
+            pass
+        super(Content, self).delete()
 
 
-class Item(Content):
-    genre = models.ForeignKey("genre")
+class Item(BaseModel):
+    """ Item
+    A generic class to store any kind of object with an HStore to store data.
+    context list the related items.
+    """
     data = hstore.DictionaryField(db_index=True)
     objects = hstore.HStoreManager()
-    context = models.ManyToManyField('self', through='Relationship',
+    context = models.ManyToManyField('self', through='Relation',
                                      symmetrical=False,
                                      related_name='related_to')
 
     class Meta:
         verbose_name = _("item")
 
+    def get(self, key):
+        try:
+            return self.data[key]
+        except:
+            return None
+
     def __unicode__(self):
-        return self.genre.name
+        return self.get('name')
 
 
-class Relationship(models.Model):
+class Relation(BaseModel):
+    """ Relation
+    A class describing a relation between two items.
+    """
     from_item = models.ForeignKey(Item, related_name='from_item')
     to_item = models.ForeignKey(Item, related_name='to_item')
-    status = models.IntegerField()
 
     data = hstore.DictionaryField(db_index=True)
     objects = hstore.HStoreManager()
-
-
