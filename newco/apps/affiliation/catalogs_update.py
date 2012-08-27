088fe563c0dccb84c22cca430eaca0e0998c7a9c
Guillaume Andreu Sabater <guillaume.andreu.sabater@gmail.com>
2012-08-27 16:18:16 +0200

    fixed function import in order to run decathlon update in standalone mode (meaning using juste the command "python <file>")

--- newco/apps/affiliation/catalogs_update.py
+++ newco/apps/affiliation/catalogs_update.py
@@ -1,13 +1,21 @@
 import sys
 from django.conf import settings
-from affiliation.decathlon.utils import decathlon_db_processing
 
 
 def main():
-    errors = decathlon_db_processing(settings.PROJECT_ROOT + "/cat_db_log.txt")
+    if settings.DEBUG:
+        pass
 
-    if len(errors) != 0:
-        sys.stdout.writelines(errors)
+    try:
+        from affiliation.decathlon.utils import decathlon_db_processing
+    except ImportError:
+        sys.stdout.write("Decathlon function import failed.\n")
+    else:
+        errors = decathlon_db_processing(
+                                    settings.PROJECT_ROOT + "/cat_db_log.txt")
+
+        if len(errors) != 0:
+            sys.stdout.writelines(errors)
 
 
 if __name__ == "__main__":
