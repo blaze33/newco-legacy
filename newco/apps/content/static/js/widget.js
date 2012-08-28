a66bfea3589eddc5fa90d64181c765a2b72bae91
Maxime Rouyrre <rouyrre+newco@gmail.com>
2012-08-28 17:32:19 +0200

    Add an addField funciton.

--- newco/apps/content/static/js/widget.js
+++ newco/apps/content/static/js/widget.js
@@ -1,7 +1,12 @@
 // js is ready if we need to do something dynamic
+function addField(selector) {
+	$(selector).append('<input type="text" name="json_key[data]" value=""> ')
+	.append('<input type="text" name="json_value[data]" value=""><br>');
+}
+
 $(function(){
 	$("#add_field").click(function() {
-		$("#form_data p").append('<input type="text" name="json_key[data]" value=""> <input type="text" name="json_value[data]" value=""><br>');
+		addField("#form_data p");
 		return false;
 	})
 });
