3fc1499357810a205a0543de347b07f3fece2196
Maxime Rouyrre <rouyrre+newco@gmail.com>
2012-09-06 12:45:31 +0200

    Bugfix: do not try to fetch album images before they're saved + PEP8.

--- newco/apps/content/transition.py
+++ newco/apps/content/transition.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from items.models import Item as LegacyItem
-from content.models import Item, Relation
+from content.models import Item
 from utils.apiservices import search_images
 
 
@@ -29,11 +29,12 @@ def add_images(request, **kwargs):
     album = product.get_items(data__contains=album_data)
     if not album:
         album = Item.objects.create(data=album_data)
+        images = []
     else:
         album = album[0]
+        images = get_album(legacy_product)
     product.link(album, {'relationship': 'has'})
 
-    images = get_album(legacy_product)
     if not images:
         images = search_images(product.data['name'])
     id_order = []
@@ -50,16 +51,18 @@ def add_images(request, **kwargs):
         album.link(image, {'relationship': 'contains',
             'order': unicode(img_order.index(x))})
         id_order.append(image.id)
-    album.data['id_order']=unicode(id_order)
+    album.data['id_order'] = unicode(id_order)
     album.save()
+
 
 def get_album(instance):
     item = sync_products(LegacyItem, instance)
-    album = item.get_items(data__contains={'class':'image_set'})[0]
-    images = album.get_items()
-    ids = [int(x) for x in album.data['id_order'].strip('[]').split(',')]
+    album = item.get_items(data__contains={'class': 'image_set'})
+    if not album:
+        return []
+    ids = [int(x) for x in album[0].data['id_order'].strip('[]').split(',')]
     images = Item.objects.filter(pk__in=ids)
     images = sorted(images, key=lambda k: ids.index(k.id))
-    json  = [i.data for i in images]
+    json = [i.data for i in images]
 
     return json
