0c02afb47328c8d2fddddaed2b99bb512fcfe823
Guillaume Andreu Sabater <guillaume.andreu.sabater@gmail.com>
2012-09-06 19:35:32 +0200

    removed unused get overriding + double quotes instead of single

--- newco/apps/profiles/views.py
+++ newco/apps/profiles/views.py
@@ -4,7 +4,6 @@ from django.core.urlresolvers import reverse
 from django.db.models import Q
 from django.http import HttpResponsePermanentRedirect, HttpResponseRedirect
 from django.utils.decorators import method_decorator
-from django.views.generic.simple import direct_to_template
 from django.views.generic.edit import ProcessFormView
 
 from django.contrib.auth.decorators import login_required
@@ -22,8 +21,8 @@ from utils.tools import load_object
 class ProfileProcessFormView(ProcessFormView):
 
     def post(self, request, *args, **kwargs):
-        if 'pf_pick' in request.POST:
-            name = request.POST['pf_pick']
+        if "pf_pick" in request.POST:
+            name = request.POST["pf_pick"]
             profile_list = Profile.objects.filter(name=name)
             if profile_list.count() == 1:
                 response = profile_list[0].get_absolute_url()
@@ -41,29 +40,13 @@ class ProfileDetailView(ProfileDetailView, ProfileProcessFormView):
 
     def dispatch(self, request, *args, **kwargs):
         if self.is_profile_page:
-            profile = Profile.objects.get(pk=kwargs.pop('pk'))
-            if profile.slug and kwargs['slug'] != profile.slug:
+            profile = Profile.objects.get(pk=kwargs.pop("pk"))
+            if profile.slug and kwargs["slug"] != profile.slug:
                 url = profile.get_absolute_url()
                 return HttpResponsePermanentRedirect(url)
-            kwargs['username'] = profile.user.username
+            kwargs["username"] = profile.user.username
         return super(ProfileDetailView, self).dispatch(request,
                                                         *args, **kwargs)
-
-    def get(self, request, *args, **kwargs):
-        if self.is_profile_page:
-            return super(ProfileDetailView, self).get(self,
-                                                    request,
-                                                    *args,
-                                                    **kwargs)
-        elif request.user.is_authenticated():
-            self.template_name = "profiles/profile_homepage.html"
-            self.page_user = request.user
-            self.object = self.page_user.get_profile()
-            context = self.get_context_data()
-            context.update({'kwargs': kwargs})
-            return self.render_to_response(context)
-        else:
-            return direct_to_template(request, "homepage.html")
 
     def get_context_data(self, **kwargs):
         #TODO: better handling of QueryManager
@@ -76,10 +59,10 @@ class ProfileDetailView(ProfileDetailView, ProfileProcessFormView):
         )
 
         fwers_ids = Follow.objects.get_follows(
-                self.page_user).values_list('user_id', flat=True)
+                self.page_user).values_list("user_id", flat=True)
         obj_fwed = Follow.objects.filter(user=self.page_user)
-        fwees_ids = obj_fwed.values_list('target_user_id', flat=True)
-        items_fwed_ids = obj_fwed.values_list('target_item_id', flat=True)
+        fwees_ids = obj_fwed.values_list("target_user_id", flat=True)
+        items_fwed_ids = obj_fwed.values_list("target_item_id", flat=True)
 
         feed = Content.objects.filter(
                 Q(author__in=fwees_ids) | Q(items__in=items_fwed_ids),
@@ -87,26 +70,26 @@ class ProfileDetailView(ProfileDetailView, ProfileProcessFormView):
         )
 
         profiles = Profile.objects.order_by("name").distinct("name")
-        list_pf = list(profiles.values_list('name', flat=True))
+        list_pf = list(profiles.values_list("name", flat=True))
 
         context = super(ProfileDetailView, self).get_context_data(**kwargs)
         context.update({
-            'reputation': self.page_user.reputation,
-            'history': history.select_subclasses(),
-            'drafts': drafts.select_subclasses(),
-            'fwers': User.objects.filter(pk__in=fwers_ids),
-            'fwees': User.objects.filter(pk__in=fwees_ids),
-            'items_fwed': Item.objects.filter(pk__in=items_fwed_ids),
-            'data_source_profile': json.dumps(list_pf),
-            'profile_list_sorted': profiles,
-            'newsfeed': feed.select_subclasses(),
+            "reputation": self.page_user.reputation,
+            "history": history.select_subclasses(),
+            "drafts": drafts.select_subclasses(),
+            "fwers": User.objects.filter(pk__in=fwers_ids),
+            "fwees": User.objects.filter(pk__in=fwees_ids),
+            "items_fwed": Item.objects.filter(pk__in=items_fwed_ids),
+            "data_source_profile": json.dumps(list_pf),
+            "profile_list_sorted": profiles,
+            "newsfeed": feed.select_subclasses(),
         })
 
         return context
 
     @method_decorator(login_required)
     def post(self, request, *args, **kwargs):
-        if 'follow' in request.POST or 'unfollow' in request.POST:
+        if "follow" in request.POST or "unfollow" in request.POST:
             obj = load_object(request)
             success_url = request.path
             return process_following(request, obj, success_url)
@@ -135,8 +118,8 @@ class ProfileListView(ProfileListView, ProfileProcessFormView):
         return profiles
 
     def get_context_data(self, **kwargs):
-        list_pf = list(Profile.objects.all().values_list('name', flat=True))
+        list_pf = list(Profile.objects.all().values_list("name", flat=True))
 
         context = super(ProfileListView, self).get_context_data(**kwargs)
-        context.update({'data_source_profile': json.dumps(list_pf)})
+        context.update({"data_source_profile": json.dumps(list_pf)})
         return context
