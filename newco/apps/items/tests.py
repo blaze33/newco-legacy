c2d8e6e543e6c695b6f355581740ae100936788c
Maxime Rouyrre <rouyrre+newco@gmail.com>
2012-10-02 18:34:36 +0200

    Disable old tests who are blocking nosetests.

--- newco/apps/items/tests.py
+++ newco/apps/items/tests.py
@@ -9,6 +9,9 @@ from django.test import TestCase
 
 
 class SimpleTest(TestCase):
+
+    __test__ = False
+
     def test_basic_addition(self):
         """
         Tests that 1 + 1 always equals 2.
