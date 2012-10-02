0fa53eb59a99540571ca17079daf12dbf09eb6b6
Guillaume Andreu Sabater <guillaume.andreu.sabater@gmail.com>
2012-10-02 18:40:38 +0200

    reinforced class loading after typeahead through new robust method 'get_class_from_string'

--- newco/apps/utils/views/search.py
+++ newco/apps/utils/views/search.py
@@ -1,8 +1,6 @@
 import json
-import re
 
 from django.core.urlresolvers import reverse
-from django.db.models.loading import get_model
 from django.http import HttpResponse, HttpResponseRedirect
 from django.views.generic import View
 from django.views.generic.base import RedirectView
@@ -11,7 +9,7 @@ from taggit.models import Tag
 
 from items.models import Item
 from profiles.models import Profile
-from utils.tools import load_redis_engine
+from utils.tools import get_class_from_string, load_redis_engine
 
 
 class TypeaheadSearchView(RedirectView):
@@ -21,13 +19,13 @@ class TypeaheadSearchView(RedirectView):
             q = request.POST.get("q")
             obj_class = request.POST.get("obj_class", "")
             obj_id = request.POST.get("obj_id", "")
-            if obj_class and obj_id:
-                obj_model = get_model(*re.split(".models.", obj_class))
-                obj = obj_model.objects.get(id=obj_id)
+            cls = get_class_from_string(obj_class)
+            if cls and obj_id:
+                obj = cls.objects.get(id=obj_id)
 
-                if obj_model == Item or obj_model == Profile:
+                if cls is Item or cls is Profile:
                     response = obj.get_absolute_url()
-                elif obj_model == Tag:
+                elif cls is Tag:
                     response = reverse("tagged_items", args=[obj.slug])
             else:
                 response = "%s?q=%s" % (reverse("content_search"), q)
