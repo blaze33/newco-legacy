05a6b131fe4b3eca839048507c5f4a4877a7b1f5
sebastien_worms <sebastien.worms@gmail.com>
2012-10-08 16:25:42 +0200

    Added "templates trial tool". See the homepage => "template trial: doc" ! Hope you'll enjoy it, and we'll save time with it

--- newco/views.py
+++ newco/views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db.models import Count
 from django.views.generic import ListView
 from django.views.generic.simple import direct_to_template
@@ -8,6 +9,37 @@ from items.models import Item, Content
 class HomepageView(ListView):
 
     paginate_by = 14
+
+    def get_context_data(self, **kwargs):
+        context = super(HomepageView, self).get_context_data(**kwargs)
+
+        if settings.DEBUG:
+            temp_av_list = {
+                "homepage_products.html" : {
+                    "1" : "Template trial: doc",
+                    "2" : "Template trial: empty"
+                },
+                "homepage_contents.html" : {
+                    "1" : "Exemple !",
+                }
+            }
+
+            names = super(HomepageView, self).get_template_names()
+
+            if temp_av_list[names[0]]:
+                temp_av = temp_av_list[names[0]]
+
+                context.update({"temp_av": temp_av})
+
+        return context
+
+    def get_template_names(self):
+        names = super(HomepageView, self).get_template_names()
+
+        if "template_test" in self.request.GET:
+            names[0] = names[0] + '_' + self.request.GET['template_test']
+
+        return names
 
     def get(self, request, *args, **kwargs):
         # if request.user.is_authenticated() or "/content" in request.path:
