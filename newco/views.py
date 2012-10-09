2ea544dab2cd68b197bb92367fdffdc923ed981d
Guillaume Andreu Sabater <guillaume.andreu.sabater@gmail.com>
2012-10-09 16:48:57 +0200

    got rid of the homepage dropdown menu. #TODO: change confusing labels on products ('Popular', 'Last Added')

--- newco/views.py
+++ newco/views.py
@@ -9,20 +9,23 @@ class HomepageView(ListView):
     paginate_by = 14
 
     def get(self, request, *args, **kwargs):
-        # if request.user.is_authenticated() or "/content" in request.path:
-        if not "cat" in kwargs or kwargs.get("cat") == "popular" or \
-                kwargs.get("cat") == "last":
+        self.cat = kwargs.get("cat", "home")
+        if self.cat == "home" or self.cat == "last":
             self.queryset = Item.objects.all()
             self.template_name = "homepage_products.html"
-            if kwargs.get("cat") == "last":
-                self.queryset = self.queryset.order_by("-pub_date")
-            else:
+            if self.cat == "home":
                 self.queryset = self.queryset.annotate(
                     Count("content")).order_by("-content__count")
-        elif kwargs.get("cat") == "questions":
+            else:
+                self.queryset = self.queryset.order_by("-pub_date")
+        elif self.cat == "questions":
             self.queryset = Question.objects.annotate(
                 score=Count("answer")).filter(score__lte=0)
             self.template_name = "homepage_contents.html"
         else:
             pass
         return super(HomepageView, self).get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        kwargs.update({"cat": self.cat})
+        return super(HomepageView, self).get_context_data(**kwargs)
