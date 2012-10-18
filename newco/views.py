7266268af822a1af3890760906289a3f041f163e
Guillaume Kalaydjian <guillaume.kalaydjian@gmail.com>
2012-10-18 11:33:12 +0200

    Improved the "Answer questions" page. Remains a little issue with <hr> line on small screen.

--- newco/views.py
+++ newco/views.py
@@ -1,9 +1,14 @@
 from django.db.models import Count
 from django.views.generic import ListView
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
 
+from taggit.models import Tag
 from items.models import Item, Question
 
 from datetime import datetime, timedelta
+
+import json
 
 class HomepageView(ListView):
 
@@ -24,11 +29,32 @@ class HomepageView(ListView):
         elif self.cat == "questions":
             self.queryset = Question.objects.annotate(
                 score=Count("answer")).filter(score__lte=0)
-            self.template_name = "homepage_questions.html"
+            if "cat" in self.request.GET:
+                self.search_terms = self.request.GET.get("cat", "")
+                category=self.search_terms
+                print category
+                self.queryset=self.queryset.filter(tags__name__contains=category)
+                if self.search_terms:
+                    self.template_name = "homepage_questions.html"
+            else:
+                self.template_name = "homepage_questions.html"   
         else:
             pass
         return super(HomepageView, self).get(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         kwargs.update({"cat": self.cat})
+        tag_names=json.dumps(list(Tag.objects.all().values_list("name", flat=True)))
+        kwargs.update({"data_source_tags":tag_names,})
+        if "cat" in self.request.GET:
+            self.search_terms = self.request.GET.get("cat", "")
+            category=self.search_terms
+            kwargs.update({"category":category,})
         return super(HomepageView, self).get_context_data(**kwargs)
+    
+    def post(self, request, *args, **kwargs):
+        if "select_category_tag" in request.POST:
+            category = request.POST.get("select_category_tag")
+            response = "%s?cat=%s" % (reverse("home", kwargs={"cat":"questions"}), category)
+            return HttpResponseRedirect(response)
+        return super(HomepageView, self).post(request, *args, **kwargs)
