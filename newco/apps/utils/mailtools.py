d2d48bd3157150b336d2c6abb1a7e6b90a58924b
Guillaume Andreu Sabater <guillaume.andreu.sabater@gmail.com>
2012-11-12 20:04:57 +0100

    enabled translation for the email receiver at send_message level

--- newco/apps/utils/mailtools.py
+++ newco/apps/utils/mailtools.py
@@ -12,19 +12,17 @@ from django.contrib.auth.models import User
 
 from account.utils import user_display
 
-#from utils.models import LastMail
-from utils.templatetags.tools import get_content_source
 
-
-def send_mail(msg_sub, receiver, txt_template, html_template, context, sender):
-    # ToDo : make the subject of email translated in "send_mail", to factorize the translation in this function ? (only thing that make it not possible )
-    # translation.activate(receiver.account.language)
+def send_mail(subject, subject_kwargs, receiver, txt_template, html_template,
+              context, sender):
+    translation.activate(receiver.account.language)
+    subject = subject % subject_kwargs
     msg_txt = txt_template.render(context)
     msg_html = html_template.render(context)
 
     email = receiver.email if not settings.DEBUG else sender.email
     newco = '"NewCo" <notifications@newco-project.fr>'
-    msg = EmailMultiAlternatives(msg_sub, msg_txt, newco, [email])
+    msg = EmailMultiAlternatives(subject, msg_txt, newco, [email])
     msg.attach_alternative(msg_html, "text/html")
 
 #    waiting_time = datetime.timedelta(minutes=1)
@@ -33,7 +31,7 @@ def send_mail(msg_sub, receiver, txt_template, html_template, context, sender):
 #    if diff > waiting_time or created:
 #        last_mail.save()
     msg.send()
-    # translation.deactivate()
+    translation.deactivate()
 
 
 def mail_question_author(request, answer):
@@ -43,10 +41,8 @@ def mail_question_author(request, answer):
     answerer = answer.author
     answerer_name = user_display(answer.author)
 
-    translation.activate(receiver.account.language)
-
-    msg_subject = _("%(receiver)s, %(answerer)s has answered your question!") % \
-                {"receiver": receiver_name, "answerer": answerer_name}
+    subject = _("%(receiver)s, %(answerer)s has answered your question!")
+    subject_kwargs = {"receiver": receiver_name, "answerer": answerer_name}
 
     txt_template = get_template("mail/_answer_notification_email.txt")
     html_template = get_template("mail/_answer_notification_email.html")
@@ -58,11 +54,10 @@ def mail_question_author(request, answer):
         "receiver_url": request.build_absolute_uri(receiver.get_absolute_url()),
         "answerer": answerer_name,
         "answerer_url": request.build_absolute_uri(answerer.get_absolute_url()),
-        "message_subject": msg_subject,
+        "message_subject": subject,
     })
-    send_mail(msg_subject, receiver, txt_template, html_template, context,
-              answerer)
-    translation.deactivate()
+    send_mail(subject, subject_kwargs, receiver, txt_template, html_template,
+              context, answerer)
 
 
 def process_asking_for_help(request, question, success_url):
@@ -108,13 +103,11 @@ def process_asking_for_help(request, question, success_url):
 
 
 def mail_helper(request, receiver, requester, question):
-    translation.activate(receiver.account.language)
-
     receiver_name = user_display(receiver)
     requester_name = user_display(requester)
 
-    msg_subject = _("%(receiver)s, %(requester)s needs your help!") % \
-                    {"receiver": receiver_name, "requester": requester_name}
+    subject = _("%(receiver)s, %(requester)s needs your help.")
+    subject_kwargs = {"receiver": receiver_name, "requester": requester_name}
 
     txt_template = get_template("mail/_ask_notification_email.txt")
     html_template = get_template("mail/_ask_notification_email.html")
@@ -125,25 +118,23 @@ def mail_helper(request, receiver, requester, question):
         "asker": requester_name,
         "asker_url": request.build_absolute_uri(requester.get_absolute_url()),
         "request": request,
-        "message_subject": msg_subject,
+        "message_subject": subject,
         "question": question,
         "question_url": request.build_absolute_uri(
             question.get_absolute_url()),
         "settings_url": request.build_absolute_uri(reverse("account_settings"))
     })
 
-    send_mail(msg_subject, receiver, txt_template, html_template, context,
-              requester)
-    translation.activate(requester.account.language)
+    send_mail(subject, subject_kwargs, receiver, txt_template, html_template,
+              context, requester)
 
 
 def mail_followee(request, fwee, fwer):
-    translation.activate(fwee.account.language)
-    fwee_name = user_display(fwee)
-    fwer_name = user_display(fwer)
+    followee_name = user_display(fwee)
+    follower_name = user_display(fwer)
 
-    msg_subject = "%s, %s is now following you on NewCo!" % \
-                  (fwee_name, fwer_name)
+    subject = _("%(followee)s, %(follower)s is now following you on NewCo.")
+    subject_kwargs = {"followee": followee_name, "follower": follower_name}
 
     txt_template = get_template("mail/_follow_notification_email.txt")
     html_template = get_template("mail/_follow_notification_email.html")
@@ -154,8 +145,8 @@ def mail_followee(request, fwee, fwer):
         "followee_url": request.build_absolute_uri(fwee.get_absolute_url()),
         "follower_url": request.build_absolute_uri(fwer.get_absolute_url()),
         "request": request,
-        "message_subject": msg_subject
+        "message_subject": subject
     })
 
-    send_mail(msg_subject, fwee, txt_template, html_template, context, fwer)
-    translation.activate(fwer.account.language)
+    send_mail(subject, subject_kwargs, fwee, txt_template, html_template,
+              context, fwer)
