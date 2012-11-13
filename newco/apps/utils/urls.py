ffa1f6576283281960564151fa8d8b1e131d13ae
Guillaume Andreu Sabater <guillaume.andreu.sabater@gmail.com>
2012-11-13 19:05:40 +0100

    replaced single by double quotes

--- newco/apps/utils/urls.py
+++ newco/apps/utils/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from utils.views.search import RedisView, TypeaheadSearchView
 
-urlpatterns = patterns('',
+urlpatterns = patterns("",
     url(r"^/redis", RedisView.as_view(), name="redis"),
     url(r"^/typeahead", TypeaheadSearchView.as_view(), name="search"),
 )
