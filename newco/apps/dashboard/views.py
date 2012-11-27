ea47fc563fc3fea903972146c2c439ef5c56d503
Guillaume Andreu Sabater <guillaume.andreu.sabater@gmail.com>
2012-11-27 20:33:19 +0100

    dashboard: removed unused import

--- newco/apps/dashboard/views.py
+++ newco/apps/dashboard/views.py
@@ -8,7 +8,6 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 
 from follow.models import Follow
-from voting.models import Vote
 
 from items.models import Content, Item
 from profiles.models import Profile
