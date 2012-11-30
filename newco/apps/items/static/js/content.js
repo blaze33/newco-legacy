4af6ebd316bde852cdb12ca406fe50d48d6ccd5f
Maxime Rouyrre <rouyrre+git@gmail.com>
2012-11-30 18:52:28 +0100

    items: image gallery wip

--- newco/apps/items/static/js/content.js
+++ newco/apps/items/static/js/content.js
@@ -3,7 +3,7 @@ $(function(){
     $('#modal-gallery').on('load', function () {
         var modalData = $(this).data('modal');
         var context = $(modalData.$links[modalData.options.index]).data('context');
-        $('#modal-gallery .modal-download span').text(context.trim().substring(0,30) + (context.length > 60 ? '...':''));
+        $('#modal-gallery .modal-download span').text(context.trim());
         $('#modal-gallery .modal-download').attr('href', context);
     });
 });
