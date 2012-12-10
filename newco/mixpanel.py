745b579289f01fac13d54fec153e628952215f81
Guillaume Kalaydjian <guillaume.kalaydjian@gmail.com>
2012-12-10 18:09:15 +0100

    Updated mixpanel file to import data from Mixpanel API

--- newco/mixpanel.py
+++ newco/mixpanel.py
@@ -4,6 +4,8 @@
 #
 # Python API client library to consume mixpanel.com analytics data.
 
+import csv
+import tablib
 import hashlib
 import urllib
 import time
@@ -12,6 +14,39 @@ try:
     import json
 except ImportError:
     import simplejson as json
+
+#Newco Dev
+#API_KEY = 'c5feefa79919576098a2f711dc1bbcc0'
+#API_SECRET = 'd39bf6955dc0f876e4687df87d6579c3'
+
+#Newco Production
+API_KEY = '36bb00d6e2f24b7f7c1f4871004d23a8'
+API_SECRET = '16be758f6a25a88315628c01eb498d52'
+
+
+TYPE = 'segmentation'
+EVENT = 'Load item detail'
+PROPERTY = 'properties["product"]'
+FROM_DATE = '2012-11-9'
+TO_DATE = '2012-11-23'
+NUM_DAYS = 15
+
+#data = csv.reader(open('products.csv','rU'))
+## Read the column names from the first line of the file 
+#fields = data.next() 
+#LIST_PRODUCTS=fields
+#
+#data = csv.reader(open('merchants.csv','rU'))
+## Read the column names from the first line of the file 
+#fields = data.next() 
+#LIST_MERCHANTS=fields
+#
+#data = csv.reader(open('referrer_page.csv','rU'))
+## Read the column names from the first line of the file 
+#fields = data.next() 
+#LIST_REFERRER=fields
+#
+#LIST=LIST_REFERRER
 
 class Mixpanel(object):
 
@@ -88,19 +123,60 @@ class Mixpanel(object):
 
 if __name__ == '__main__':
     api = Mixpanel(
-        api_key = 'c5feefa79919576098a2f711dc1bbcc0', 
-        api_secret = 'd39bf6955dc0f876e4687df87d6579c3'
+        api_key = API_KEY, 
+        api_secret = API_SECRET
     )
-    data = api.request(['segmentation',],{
-        'event' : 'Click to Product',
-        'from_date':'2012-11-03',
-        'to_date':'2012-11-08',
-        'on':'properties["product"]',
+    
+    
+    
+    
+    
+    #### Segmentation on Properties ####
+    data = api.request([TYPE,],{
+        'event' : EVENT,
+        'from_date':FROM_DATE,
+        'to_date':TO_DATE,
+        'on':PROPERTY,
         #'name':'Country',
         #'values':['Firefox'],
         #'unit' : 'day',
         #'interval' : 7,
-        #'type': 'general'
+        'type': 'unique'
     })
-    pprint.pprint( data)
+    #print LIST
+    pprint.pprint(data)
+    filename = 'results.%s.%06d.csv' %(FROM_DATE+'_'+TO_DATE+EVENT+'_'+PROPERTY,2012)
+    csv_file = open(filename, 'wb')
+    wr = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+    header=["Dates"]
+    d = data['data'] ['values']
+    for key, value in d.iteritems():
+            header.append(key)
+    wr.writerow(header)
+    
+    #### Segmentation/Properties ####
+    
+    for i in range(NUM_DAYS):
+        
+        date = data['data'] ['series'] [i]
+        list = [date]
+        for key, value in d.iteritems():
+            value=data['data'] ['values'] [key][date]
+            list.append(value)
+        wr.writerow(list)
+        
+        
+#        for j in LIST:
+#            value=data['data'] ['values'] [j][date]
+#            list.append(value)
+        #value_1 = data['data'] ['values'] ['Amazon'][date]
+        #value_2 = data['data'] ['values'] ['Decathlon'][date]    
+    
+    #### Segmentation Click to Product ####
+#    for i in range(11):
+#        date = data['data'] ['series'] [i]
+#        value = data['data'] ['values'] ['Click to Product'][date]
+#        wr.writerow([date,value])
+    
+    #pprint.pprint( data_csv)
 #pprint.pprint (request)
