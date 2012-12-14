387820f2f60652f750477e4f090ea2be65ab9031
Guillaume Andreu Sabater <guillaume.andreu.sabater@gmail.com>
2012-12-14 02:08:51 +0100

    dashboard: improved jslint compliance

--- newco/apps/dashboard/static/js/dashboard.js
+++ newco/apps/dashboard/static/js/dashboard.js
@@ -1,31 +1,31 @@
 $(function() {
     var boxCol = $('div[id^="box_"]');
     $.each(boxCol, function (i, item) {
-        var btn_icon = $('[rel=collapse_btn]', item);
-        var div = $('[rel=collapse_div]', item);
+        var btn_icon, div;
+        btn_icon = $('[rel=collapse_btn]', item);
+        div = $('[rel=collapse_div]', item);
         div.on('shown', function () {
             btn_icon.removeClass('icon-plus').addClass('icon-minus');
-        })
+        });
         div.on('hidden', function () {
             btn_icon.removeClass('icon-minus').addClass('icon-plus');
-        })
-    })
+        });
+    });
 });
 
 function TriggerMasonry_boxes() {
-    var $container = $('#boxes_container');
-    $container.masonry({
+    $('#boxes_container').masonry({
         itemSelector : '.box',
         isAnimated: true,
         // Container is span10 => col = container / 10
         columnWidth: function( containerWidth ) {
             return containerWidth / 10;
-        },
+        }
     });
 }
 
-$(function()    {
-    TriggerMasonry_boxes()
-    $('[rel=collapse_div]').on('hidden',function() { TriggerMasonry_boxes() });
-    $('[rel=collapse_div]').on('shown',function() { TriggerMasonry_boxes() });
+$(function() {
+    TriggerMasonry_boxes();
+    $('[rel=collapse_div]').on('hidden',function() { TriggerMasonry_boxes(); });
+    $('[rel=collapse_div]').on('shown',function() { TriggerMasonry_boxes(); });
 });
