98d05cc1f67fc758db5a88c2738373c0686b1b23
Guillaume Andreu Sabater <guillaume.andreu.sabater@gmail.com>
2013-01-02 17:23:28 +0100

    utils: added error handling when fetching images from google

--- newco/apps/utils/apiservices.py
+++ newco/apps/utils/apiservices.py
@@ -1,5 +1,7 @@
 import requests
+
 from django.conf import settings
+
 
 def search_images(query):
     payload = {
@@ -7,14 +9,19 @@ def search_images(query):
         'cx': settings.GOOGLE_SEARCH_ENGINE_ID,
         'key': settings.GOOGLE_API_KEY,
         'searchType': 'image',
-        }
-    r = requests.get("https://www.googleapis.com/customsearch/v1", params=payload)
-
+    }
+    r = requests.get("https://www.googleapis.com/customsearch/v1",
+                     params=payload)
+    if not r.ok:
+        if r.status_code == 403:
+            # send msg to logger
+            pass
     return flatten(r)
+
 
 def flatten(r):
     flat = []
-    for i in r.json['items']:
+    for i in r.json.get("items", []):
         i.update(i.pop('image'))
         flat.append(i)
     return flat
