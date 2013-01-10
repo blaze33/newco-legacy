6f2f0b089bddbb625d68d4f7a378328efa7e7208
Guillaume Andreu Sabater <guillaume.andreu.sabater@gmail.com>
2013-01-10 18:04:58 +0100

    items: added js code pre-filling modal

--- newco/apps/items/static/js/content.js
+++ newco/apps/items/static/js/content.js
@@ -1,9 +1,20 @@
 // js is ready if we need to do something dynamic
-$(function(){
+$(function() {
     $('#modal-gallery').on('load', function () {
-        var modalData = $(this).data('modal');
-        var context = $(modalData.$links[modalData.options.index]).data('context');
+        var modalData, context;
+        modalData = $(this).data('modal');
+        context = $(modalData.$links[modalData.options.index]).data('context');
         $('#modal-gallery .modal-download span').text(context.trim());
         $('#modal-gallery .modal-download').attr('href', context);
+    });
+});
+
+$(function() {
+    $(document).on("click", ".ask-btn", function () {
+        var questionId, modalAsk;
+        questionId = $(this).data("question-id");
+        modalAsk = $("#modal-ask");
+        $("#question-id", modalAsk).val(questionId);
+        modalAsk.modal("show");
     });
 });
