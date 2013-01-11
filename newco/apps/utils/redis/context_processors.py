29a112192e3686b340f715427c1f60c7fbf2ada5
Guillaume Andreu Sabater <guillaume.andreu.sabater@gmail.com>
2013-01-11 13:11:26 +0100

    redis: fixed typo

--- newco/apps/utils/redis/context_processors.py
+++ newco/apps/utils/redis/context_processors.py
@@ -5,5 +5,5 @@ def redis_url(request):
     return {
         "URL_REDIS": reverse_lazy("redis"),
         "URL_REDIS_TAG": reverse_lazy("redis", args=["tag"]),
-        "URL_REDIS_PROFILE": reverse_lazy("redis", args=["tag"]),
+        "URL_REDIS_PROFILE": reverse_lazy("redis", args=["profile"]),
     }
