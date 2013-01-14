4dd43cfee81bbefccfc0c90ddc4a894201b3270f
Guillaume Andreu Sabater <guillaume.andreu.sabater@gmail.com>
2013-01-14 15:13:54 +0100

    utils: added the ability to ask for help with just an email address

--- newco/apps/utils/views/help.py
+++ newco/apps/utils/views/help.py
@@ -1,17 +1,25 @@
 import re
 
+from django.conf import settings
 from django.http import HttpResponseRedirect
 from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext_lazy as _
 from django.views.generic.base import RedirectView
 
 from django.contrib import messages
+from django.contrib.auth.models import User
 
+from account.models import Account
 from account.utils import user_display
 
 from items.models import Question
 from profiles.models import Profile
+from utils.constants import EMAIL_PATTERN
 from utils.mailtools import mail_helper
+
+
+def void():
+    return ""
 
 
 class AskForHelpView(RedirectView):
@@ -21,32 +29,44 @@ class AskForHelpView(RedirectView):
             return super(AskForHelpView, self).post(request, *args, **kwargs)
 
         question_id = request.POST.get("question-id", "")
-
-        profile_ids = []
-        if request.POST["ask"] == "expert":
-            profile_ids.append(int(request.POST["expert-id"]))
-        elif request.POST["ask"] == "users":
-            profile_ids.extend(
-                map(int, re.split("\D+", request.POST["profile-ids"])))
-
-        if not question_id or not profile_ids:
+        if not question_id:
             # sent to logger + fails silenlty
             return HttpResponseRedirect(request.path)
 
+        case = request.POST["ask"]
         question = Question.objects.get(id=question_id)
         username = user_display(request.user)
+        profile_ids = []
 
-        receiver_profiles = Profile.objects.filter(
-            id__in=profile_ids).select_related("user")
+        if case == "email":
+            receiver_profiles = []
+            m = EMAIL_PATTERN.match(request.POST["email"])
+            if m:
+                receiver_profile = Profile(user=User(email=m.group()),
+                                           name=m.group("username"))
+                receiver_profile.user.account = Account()
+                receiver_profile.user.get_absolute_url = void
+                receiver_profiles.append(receiver_profile)
+            else:
+                kwargs = {"user": username, "email": request.POST["email"]}
+                display_message("email", request, **kwargs)
+        elif case == "expert" or case == "users":
+            if case == "expert":
+                profile_ids.append(int(request.POST["expert-id"]))
+            elif case == "users":
+                profile_ids.extend(
+                    map(int, re.split("\D+", request.POST["profile-ids"])))
+            receiver_profiles = Profile.objects.filter(
+                id__in=profile_ids).select_related("user")
+
         for receiver_profile in receiver_profiles:
             receiver = receiver_profile.user
 
-            # Not worth reloading profile instance
-            # receiver_name = user_display(receiver_profile)
-            receiver_name = receiver_profile.name
+            receiver_name = user_display(receiver_profile)
 
             if receiver != request.user:
-                mail_helper(request, receiver, request.user, question)
+                mail_helper(request, receiver, request.user, question,
+                            receiver_name, username)
                 kwargs = {"user": username, "receiver": receiver_name}
                 display_message("sent", request, **kwargs)
             else:
@@ -63,6 +83,10 @@ MESSAGES = {
     "warning": {
         "level": messages.WARNING,
         "text": _("{user}, you can't ask yourself to answer a question!")
+    },
+    "email": {
+        "level": messages.ERROR,
+        "text": _("{user}, {email} is not a valid email address!")
     },
 }
 
