558fa05168dcde984387f2a6951f74ca251bb878
Guillaume Andreu Sabater <guillaume.andreu.sabater@gmail.com>
2013-01-22 18:55:32 +0100

    items: restored question form related js

---
+++ newco/apps/items/static/js/partial_question_form.js
@@ -0,0 +1,8 @@
+$(function() {
+    $("#question-form").on("show hidden", function (eventObject) {
+        $("#question-icon").toggleClass("icon-plus icon-remove");
+        $("#question-text").toggle();
+        if ( eventObject.type === "show" ) { $("#id_question-content").focus(); }
+    });
+});
+
