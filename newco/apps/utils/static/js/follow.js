ac4675aa5d0acd06022dea587ecbe322f1f51d67
Guillaume Andreu Sabater <guillaume.andreu.sabater@gmail.com>
2013-01-28 14:00:48 +0100

    follow: added static file

---
+++ newco/apps/utils/static/js/follow.js
@@ -0,0 +1,22 @@
+/*global displayMessage*/
+
+(function ($) {
+    "use strict";
+    /*jslint browser:true, devel: true*/
+
+    $(".form-follow").submit(function () {
+        console.log("caca");
+        $.ajax({
+            type: "post",
+            data: $(this).serialize() + "&follow",
+            success: function(data, textstatus, jqxhr) {
+                $("[name=follow]").toggle();
+                // displayMessage(data.message);
+            },
+            error: function(xhr, status, error) {
+                console.log(xhr, status, error);
+            }
+        });
+        return false;
+    });
+}(window.jQuery));
