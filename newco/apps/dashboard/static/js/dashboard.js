f1ea84f48c31e6bbc84b052cd17a6f568bf39443
Guillaume Andreu Sabater <guillaume.andreu.sabater@gmail.com>
2013-02-05 18:29:53 +0100

    dashboard: factorized js

--- newco/apps/dashboard/static/js/dashboard.js
+++ newco/apps/dashboard/static/js/dashboard.js
@@ -10,10 +10,7 @@
     $.triggerMasonry($("#boxes"), ".box", extraOptions);
     $(".collapse").on("shown hidden", function () {
         $.triggerMasonry($("#boxes"), ".box", extraOptions);
-    });
-    $.each($(".box"), function (index, Element) {
-        $(".collapse", Element).on("shown hidden", function () {
-            $("button i", Element).toggleClass("icon-plus icon-minus");
-        });
+        var parentBox = $(this).closest(".box");
+        $("button i", parentBox).toggleClass("icon-plus icon-minus");
     });
 }(window.jQuery));
