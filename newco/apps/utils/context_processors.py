52a9dbfbbcdbf9b9284b3003890945a6f7a88aaa
Guillaume Andreu Sabater <guillaume.andreu.sabater@gmail.com>
2013-02-11 12:53:25 +0100

    mixpanel: sent user info template side via context processor/json. @guillaumekal up to you now

--- newco/apps/utils/context_processors.py
+++ newco/apps/utils/context_processors.py
@@ -1,6 +1,11 @@
+import json
+
 from django.conf import settings
+from django.core.serializers.json import DjangoJSONEncoder
 
 from django.contrib.sites.models import Site
+
+from account.utils import user_display
 
 
 def site_settings(request):
@@ -12,6 +17,15 @@ def site_settings(request):
 
 
 def settings_mp(request):
-    return {
-        "MIXPANEL_KEY_ID": settings.MIXPANEL_KEY_ID,
-    }
+    output = {"MIXPANEL_KEY_ID": settings.MIXPANEL_KEY_ID}
+    user = request.user
+    if user.is_authenticated():
+        profile = user.get_profile()
+        data = {"userId": user.id, "email": user.email, "bio": profile.about,
+                "created": user.date_joined, "name": user_display(profile),
+                "reputation": user.reputation.reputation_incremented}
+    else:
+        data = {"userId": 0}
+    output = {"MIXPANEL_KEY_ID": settings.MIXPANEL_KEY_ID,
+              "user_json": json.dumps(data, cls=DjangoJSONEncoder)}
+    return output
