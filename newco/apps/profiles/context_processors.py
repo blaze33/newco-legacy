601f2ed3285a30a2d228b6a56bc9e4a1ae7be2b5
Guillaume Andreu Sabater <guillaume.andreu.sabater@gmail.com>
2013-02-13 21:30:43 +0100

    profiles: fixed context processor that requires to return a dictionary

--- newco/apps/profiles/context_processors.py
+++ newco/apps/profiles/context_processors.py
@@ -2,9 +2,8 @@ from account.utils import user_display
 
 
 def profile(request):
+    result = {}
     if request.user.is_authenticated():
         profile = request.user.get_profile()
-        return {
-            "PROFILE": profile,
-            "USERNAME": user_display(profile)
-        }
+        result.update({"PROFILE": profile, "USERNAME": user_display(profile)})
+    return result
