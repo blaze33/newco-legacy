170df0b9bd3ee9f693bf05a4c5ae2da1766d9535
Maxime Rouyrre <rouyrre+git@gmail.com>
2013-03-04 23:56:52 +0100

    utils: clean_content refactor, avoid deleting too many images

--- newco/apps/utils/scripts/clean_content.py
+++ newco/apps/utils/scripts/clean_content.py
@@ -3,30 +3,34 @@ from django.conf import settings
 print "Using " + settings.DATABASES["default"]["NAME"] + """ database.
 node products with deleted legacy items are deleted.
 """
-from content.models import Item as cItem
+from content.models import GraphQuery, Item as cItem
 from items.models import Item
 
-ids = map(lambda x: int(x.data['legacy_id']), cItem.objects.hfilter({'_class': 'product'}))
-exids = map(lambda x: x.id, Item.objects.filter(pk__in=ids))
-id666 = [i for i in ids if i not in exids]
 
+def clean_nodes_products():
+    ids = map(lambda x: int(x.data['legacy_id']), cItem.objects.hfilter({'_class': 'product'}))
+    legacy_ids = map(lambda x: x.id, Item.objects.filter(pk__in=ids))
+    deleted_ids = [i for i in ids if i not in legacy_ids]
 
-def deleted_legacy(x):
-    global id666
-    return True if int(x.legacy_id) in id666 else False
+    old = [x for x in cItem.objects.hfilter({'_class': 'product'}).hselect(['legacy_id'])
+           if int(x.legacy_id) in deleted_ids]
+    print 'Those items were already deleted:', old, '\n'
 
-old = filter(deleted_legacy, cItem.objects.hfilter({'_class': 'product'}).hselect(['legacy_id']))
-print 'Those items were already deleted:', old
+    n_img, n_album = 0, 0
+    for i in old:
+        if i.successors.all():
+            n_album += i.successors.count()
+            i.successors.all().delete()
+        i.delete()
 
-n_img, n_album = 0, 0
-for i in old:
-    if i.successors.all():
-        n_img += i.successors.all()[0].successors.all().count()
-        n_album += 1
-        i.successors.all()[0].successors.all().delete()
-        i.successors.all().delete()
-    i.delete()
+    n_img = GraphQuery().isolated().count()
+    GraphQuery().isolated().delete()
 
-print 2 * n_album + n_img, 'nodes deleted:'
-print n_album, 'product nodes,', n_album, 'album nodes,', n_img, 'image nodes'
-print 'many relations deleted'
+    print 2 * n_album + n_img, 'nodes deleted:', \
+        n_album, 'product nodes,', \
+        n_album, 'album nodes,', \
+        n_img, 'image nodes'
+    print 'many relations deleted'
+
+if __name__ == "__main__":
+    clean_nodes_products()
