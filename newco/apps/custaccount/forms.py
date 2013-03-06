c0bb4d538e698b6d701500d6f7610a07ee2463e0
Guillaume Andreu Sabater <guillaume.andreu.sabater@gmail.com>
2013-03-06 19:12:52 +0100

    account|signup: added first and last name fields
    no validation provided. quick and simple. needs some extra work

--- newco/apps/custaccount/forms.py
+++ newco/apps/custaccount/forms.py
@@ -6,18 +6,26 @@ from account.forms import SignupForm
 
 class SignupForm(SignupForm):
 
+    first_name = CharField(
+        label=_("First name") + " *", max_length=30, required=False,
+        error_messages={"max_length": _("No more than 30 characters.")})
+    last_name = CharField(
+        label=_("Last name") + " *", max_length=30, required=False,
+        help_text=_("* Optionnal"),
+        error_messages={"max_length": _("No more than 30 characters.")})
+
     profile_name = CharField(
         label=_("Profile name"), max_length=30, help_text=_(
             "30 characters tops. Can be whatever you want. "
             "And can be modified."),
-        error_messages={"max_length": _("No more than 30 characters.")}
-    )
+        error_messages={"max_length": _("No more than 30 characters.")})
 
     class Meta:
-        fields = ('email', 'password1', 'password2', 'profile_name')
+        fields = ("email", "password1", "password2",
+                  "first_name", "last_name", "profile_name")
 
     def __init__(self, *args, **kwargs):
         super(SignupForm, self).__init__(*args, **kwargs)
         del self.fields["username"]
         self.fields.keyOrder = ["email", "password", "password_confirm",
-                                "profile_name"]
+                                "first_name", "last_name", "profile_name"]
