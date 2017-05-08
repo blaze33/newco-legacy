842dbf097e47344186f3856679b9bc382b2a95cd
Maxime Rouyrre <rouyrre+git@gmail.com>
2017-05-08 13:41:07 +0200

    Updated website URL !

--- newco/apps/utils/middleware.py
+++ newco/apps/utils/middleware.py
@@ -13,11 +13,11 @@ class MyMiddleware(object):
     def process_request(self, request):
         host = request.get_host()
         old_url = [host, request.path]
-        new_host = "http://newco-prod.herokuapp.com"
+        new_host = "http://newco.openbloc.fr"
 
         if 'google009a062b2cd3a7e6.html' in request.path:
             return
-        if "newco-project" in old_url[0]:
+        if "newco-project" in old_url[0] or "herokuapp" in old_url[0]:
             return HttpResponsePermanentRedirect(new_host + request.path)
         # No redirects required.
         return
